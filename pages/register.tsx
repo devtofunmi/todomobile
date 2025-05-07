6c54199f0654cc9bdb5e2236776777add01ec743
DevTofunmi <olayiwolajesutofunmi@gmail.com>
2025-05-07 23:20:39 +0100

    Update register.tsx

--- pages/register.tsx
+++ pages/register.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Register() {
   return (
-    <div className="h-screen relative bg-[#eff4f3] text-black flex flex-col justify-between px-6 pt-10 pb-6">
+    <div className="h-screen relative bg-[#eff4f3] font-poppins text-black flex flex-col justify-between px-6 pt-10 pb-6">
       <div className="absolute top-0 left-0">
         <img src="/shape.png" alt="shapeicon" className="w-40" />
       </div>
