c33b545568ab5101d4ba945c06220a9e305f54d6
DevTofunmi <olayiwolajesutofunmi@gmail.com>
2025-05-07 23:20:02 +0100

    Update Input.tsx

--- components/Input.tsx
+++ components/Input.tsx
@@ -10,7 +10,7 @@ interface InputProps {
         <input
           type={type}
           placeholder={placeholder}
-          className="w-full rounded-full bg-white   px-6 py-4 text-sm placeholder:text-sm placeholder-black/80 focus:outline-none focus:ring-2 focus:ring-cyan-400"
+          className="w-full font-poppins rounded-full bg-white   px-6 py-4 text-sm placeholder:text-sm placeholder-black/80 focus:outline-none focus:ring-2 focus:ring-cyan-400"
           {...props}
         />
       </div>
