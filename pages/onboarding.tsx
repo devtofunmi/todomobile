0588a5b6d223cf1d37173ba2a9d08a5f312b4a2e
DevTofunmi <olayiwolajesutofunmi@gmail.com>
2025-05-08 00:38:48 +0100

    Update onboarding.tsx

--- pages/onboarding.tsx
+++ pages/onboarding.tsx
@@ -3,36 +3,31 @@ import Link from "next/link";
 
 export default function Onboarding() {
   return (
-    <div className="h-screen w-full font-poppins bg-[#eff4f3] px-6 pt-10 pb-6 relative">
+    <div className="h-screen bg-[#eff4f3] font-poppins text-black flex flex-col px-6 pt-10 pb-6 relative">
       <div className="absolute top-0 left-0">
         <img src="/shape.png" alt="shapeicon" className="w-40" />
       </div>
 
-      <div className="max-w-md mx-auto flex flex-col justify-center">
-        <div className="flex flex-col items-center justify-center text-center mt-12">
-          <img src="/onboard.png" alt="Onboarding" className="w-40 mb-6" />
-          <h1 className="text-xl font-bold mb-2 text-black">
-            Gets things with TODOs
-          </h1>
-          <p className="text-gray-600 mb-6 leading-relaxed">
-            Lorem ipsum dolor sit amet<br />
-            consectetur. Eget sit nec et<br />
-            euismod. Consequat urna<br />
-            quam felis interdum quisque.<br />
-            Malesuada adipiscing tristique<br />
-            ut eget sed.
-          </p>
-        </div>
-        </div>
-        <div className="w-full max-w-sm mx-auto  flex flex-col gap-4">
+      {/* Main content */}
+      <div className="flex flex-col flex-grow justify-center items-center text-center w-full max-w-md mx-auto mt-12">
+        <img src="/onboard.png" alt="Onboarding" className="w-40 mb-6" />
+        <h1 className="text-xl font-bold mb-2">Gets things with TODOs</h1>
+        <p className="text-gray-600 mb-6 leading-relaxed">
+          Lorem ipsum dolor sit amet<br />
+          consectetur. Eget sit nec et<br />
+          euismod. Consequat urna<br />
+          quam felis interdum quisque.<br />
+          Malesuada adipiscing tristique<br />
+          ut eget sed.
+        </p>
+      </div>
 
-        <div className="w-full">
-          <Link href="/register">
-            <Button>Get Started</Button>
-          </Link>
-        </div>
-</div>
-      
+      {/* Bottom button */}
+      <div className="w-full max-w-sm mx-auto flex flex-col gap-4">
+        <Link href="/register">
+          <Button>Get Started</Button>
+        </Link>
+      </div>
     </div>
   );
 }
